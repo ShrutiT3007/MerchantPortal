1b1fa127714397738102b8ed2c191ff6f3170dd3
shruti.tyagi <shruti.tyagi@freecharge.com>
2026-09-07 17:46:52 +0530

    fix

--- src/routes/index.js
+++ src/routes/index.js
@@ -19,12 +19,12 @@ router.use("/auth", authRoute);
 router.use("/health",healthCheckRoute)
 router.use("/eil", authMiddleware, eilRoute);
 router.use("/mob",authMiddleware, mobRoute);
-router.use("/sqs",authMiddleware, sqs);
+router.use("/sqs", sqs);
 router.use("/user", authMiddleware, userRoute);
 router.use("/download-history", authMiddleware, downloadHistoryRoute);
 router.use("/branch-alerts", authMiddleware, branchAlertsRoute);
 router.use("/jenkins", authMiddleware, jenkinsMappingRoute);
 router.use("/qa-testing", qaRoute);
-router.use("/pa", authMiddleware, paRoute);
+router.use("/pa", paRoute);
 
 module.exports = router;
